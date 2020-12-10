7a81b4a20d0c21da570eccde399547bccec38702
Rico <ricomitch@icloud.com>
2020-12-10 00:48:44 -0500

    set up files

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Grid from '@material-ui/core'
+
+const Products = [
+  { id: 1, name: 'Shoes', description: 'Running shoes.' },
+  { id: 2, name: 'Macbook', description: 'Apple macbook.' },
+]
+
+const Products = () => {
+  ;<main>
+    <Grid container justify='center' spacing={4}></Grid>
+  </main>
+}
+
+export default Products
