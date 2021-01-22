afc2b3c38fa98f1de0b80657ab3435194c0ea089
Rico <ricomitch@icloud.com>
2021-01-22 17:47:00 -0500

    navbar fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,12 +9,29 @@ import {
 } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
 
+import logo from '../../assets/nike-logo.png'
+import classes from '*.module.css'
+
 const Navbar = () => {
   return (
     <>
       <AppBar position='fixed' className={classes.appBar} color='inherit'>
         <ToolBar>
-          <Typography></Typography>
+          <Typography variant='h6' className={classes.title} color='inherit'>
+            <img
+              src={logo}
+              alt='Commerce.js'
+              height='25px'
+              className={classes.image}
+            />
+            Nike
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label='Show cart items' color='inherit'>
+              <Badge badgeContent={2}></Badge>
+            </IconButton>
+          </div>
         </ToolBar>
       </AppBar>
     </>
